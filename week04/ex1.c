d30e72410cc9f1a8b3915a62ae23c223ddc9e002
Slauva <koshman2015@list.ru>
2021-09-14 23:56:10 +0300

    added ex2

---
+++ week04/ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+    int n = 10;
+
+    for (int i = 0; i < n; i++) {
+        }
+
+    return 0;
+}
