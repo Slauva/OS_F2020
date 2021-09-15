fd01a206716eb51bc70e0fc43e02115694ab58de
Slava <koshman2015@list.ru>
2021-09-15 12:13:51 +0300

    Lab 4 final

--- week04/ex4.c
+++ week04/ex4.c
@@ -6,15 +6,17 @@
 
 #include "string.h"
 
-int main() {
-    while (1) {
-        char in[256];
+int main()
+{
+    while (1)
+    {
+        char line[256];
         printf("> ");
         fgets(line, sizeof line, stdin);
-        printf("> ");
 
         int pid = fork();
-        if (pid == 0) {
+        if (pid == 0)
+        {
             system(line);
         }
         waitpid(pid, NULL, 0);
